ad6f54d50795094e2f7d2f442a8162e49c437582
damirTAG <damirtagilbayev17@gmail.com>
2024-07-12 21:49:03 +0500

    init

---
+++ database/backup/__init__.py
@@ -0,0 +1,17 @@
+"""
+Backup folder to storage backup files every 24h
+type: {db_filename}_{datetime}.db
+"""
+# 
+# DATABASE FOLDER PROPERTY OF KBTU-FILES BOT ON TELEGRAM
+# DEVELOPED BY DAMIRTAG (https://t.me/damirtag)
+"""
+      _                    _        _                
+     | |                  (_)      | |               
+   __| |  __ _  _ __ ___   _  _ __ | |_  __ _   __ _ 
+  / _` | / _` || '_ ` _ \ | || '__|| __|/ _` | / _` |
+ | (_| || (_| || | | | | || || |   | |_| (_| || (_| |
+  \__,_| \__,_||_| |_| |_||_||_|    \__|\__,_| \__, |
+                                                __/ |
+                                               |___/ 
+"""
---
+++ database/data/__init__.py
@@ -0,0 +1,16 @@
+"""
+set your database files from sqlite3 here
+"""
+# 
+# DATABASE FOLDER PROPERTY OF KBTU-FILES BOT ON TELEGRAM
+# DEVELOPED BY DAMIRTAG (https://t.me/damirtag)
+"""
+      _                    _        _                
+     | |                  (_)      | |               
+   __| |  __ _  _ __ ___   _  _ __ | |_  __ _   __ _ 
+  / _` | / _` || '_ ` _ \ | || '__|| __|/ _` | / _` |
+ | (_| || (_| || | | | | || || |   | |_| (_| || (_| |
+  \__,_| \__,_||_| |_| |_||_||_|    \__|\__,_| \__, |
+                                                __/ |
+                                               |___/ 
+"""
